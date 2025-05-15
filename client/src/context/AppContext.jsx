c92ad2848f2f9d60fc9954e6a1ae2781736eab7b
supuni97 <sitharabandara1997@gmail.com>
2025-05-15 19:36:18 +0530

    Updated navbar

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,10 +6,19 @@ export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
   const navigate = useNavigate();
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(true);
   const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
 
-  const value = { navigate, user, setUser, setIsSeller, isSeller };
+  const value = {
+    navigate,
+    user,
+    setUser,
+    setIsSeller,
+    isSeller,
+    showUserLogin,
+    setShowUserLogin,
+  };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
