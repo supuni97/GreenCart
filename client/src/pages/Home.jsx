132fbbaf6bace276b28625ec99ef28d25583dbd8
supuni97 <sitharabandara1997@gmail.com>
2025-05-18 19:47:01 +0530

    Added bottom banner in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import MainBanner from "../components/MainBanner";
 import Categories from "../components/Categories";
 import BestSeller from "../components/BestSeller";
+import BottomBanner from "../components/BottomBanner";
 const Home = () => {
   return (
     <div className="mt-10">
       <MainBanner />
       <Categories />
       <BestSeller />
+      <BottomBanner />
     </div>
   );
 };
