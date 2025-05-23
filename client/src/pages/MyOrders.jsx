17b797d27b1a34d65a046095fa86bcf3395aef00
supuni97 <sitharabandara1997@gmail.com>
2025-05-23 11:50:37 +0530

    updated come css in my orders page

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -57,7 +57,7 @@ const MyOrders = () => {
                 </div>
               </div>
 
-              <div className="text-primary text-lg font-medium">
+              <div className="flex flex-col justify-center md:ml-8 md:mb-0">
                 <p>Quantity: {item.quantity || "1"}</p>
                 <p>Status: {order.status}</p>
                 <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
