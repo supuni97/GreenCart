1e197c953fc4aa8c1fa0b8f9a1c85dc8a2102c94
supuni97 <sitharabandara1997@gmail.com>
2025-05-24 18:54:37 +0530

    Added middleware configurations to server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+//allow multiple origins
+const allowedOrigins = ["http://localhost:5173"];
+
+//middleware configurations
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => res.send("API is working!"));
 
