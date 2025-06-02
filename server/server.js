781e18caafbf1ecb73061d61f46656908a80bb5b
supuni97 <sitharabandara1997@gmail.com>
2025-06-02 13:11:30 +0530

    Added routes for products

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import "dotenv/config";
 import userRouter from "./routes/userRoute.js";
 import sellerRouter from "./routes/sellerRoute.js";
 import connectCloudinary from "./configs/CLOUDINARY.JS";
+import productRouter from "./routes/productRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -26,6 +27,7 @@ app.get("/", (req, res) => res.send("API is working!"));
 
 app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
+app.use("/api/product", productRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
